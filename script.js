27096012d6483d9af9f7bcdc8d9e724b494ec149
Hanushka <andisiwenkonki.an@gmail.com>
2025-12-04 18:59:14 +0200

    File added

---
+++ script.js
@@ -0,0 +1,13 @@
+for (let index = 1; index <= 10; index++) {
+  console.log(index);
+}
+
+console.log(" ");
+
+// While loop
+ let index = 1;
+
+ while (index <= 10) {
+   console.log(index);
+   index++;
+ }
